b7e5704b529aaf355e4aa5b149f8452d278e1d9d
Evolve5164 <150050862+Evolve5164@users.noreply.github.com>
2023-12-05 21:22:15 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -45,12 +45,10 @@ function pingUrl(Url, statusElementId) {
     });
 }
 
-
-
 function copyToClipboard(element) {
     let $temp = $("<input>");
     $("body").append($temp);
-    $temp.val($(element).text()).select();
+    $temp.val("https://" + $(element).text()).select();
     document.execCommand("copy");
     $temp.remove();
     // Display a sliding notification that the text has been copied
