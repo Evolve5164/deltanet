575d7926b8bcef1132b483a24796f82df0bac2ed
Evolve5164 <150050862+Evolve5164@users.noreply.github.com>
2023-12-05 20:23:09 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
+// Set Urls
+const stream1 = "stream.deltanet.tk:55251"
+const stream2 = "deltastream.duckdns.org:55251"
+const request1 = "request.deltanet.tk:55251"
+const request2 = "deltarequest.duckdns.org:55251"
+const downloads = "jellyfin.org/downloads"
+
+// Set Urls to ping
+const stream1Ping = "https://" + stream1 + "/system/info/public";
+const stream2Ping = "https://" + stream2 + "/system/info/public";
+const request1Ping = "https://" + request1 + "/api/v1/status";
+const request2Ping = "https://" + request2 + "/api/v1/status";
+const downloadsPing = "https://" + downloads;
+
+// Set <a> href to given urls
+function setLinks() {
+    document.getElementById("stream1").textContent = stream1
+    document.getElementById("stream2").textContent = stream2
+    document.getElementById("request1").textContent = request1
+    document.getElementById("request2").textContent = request2
+    document.getElementById("downloads").textContent = downloads
+}
+
+// Set <a> href to given urls
+function setButtonLinks() {
+    document.getElementById("stream1Button").href = "https://" + stream1
+    document.getElementById("stream2Button").href = "https://" + stream2
+    document.getElementById("request1Button").href = "https://" + request1
+    document.getElementById("request2Button").href = "https://" + request2
+    document.getElementById("downloads").href = "https://" + downloads
+}
+
+function pingUrl(Url, statusElementId) {
+    fetch(Url)
+    .then(response => {
+        if (response.ok) {
+            document.getElementById(statusElementId).classList.remove("down");
+            document.getElementById(statusElementId).classList.add("up");
+        }
+    })
+    .catch(error => {
+        document.getElementById(statusElementId).classList.remove("up");
+        document.getElementById(statusElementId).classList.add("down");
+        console.error(error);
+    });
+}
+
+
+
 function copyToClipboard(element) {
     let $temp = $("<input>");
     $("body").append($temp);
@@ -10,3 +59,11 @@ function copyToClipboard(element) {
         $("#notification").css("height", "0");
     }, 3000);
 }
+
+window.onload = pingUrl(stream1Ping, "stream1Status");
+window.onload = pingUrl(stream2Ping, "stream2Status");
+window.onload = pingUrl(request1Ping, "request1Status");
+window.onload = pingUrl(request2Ping, "request2Status");
+window.onload = pingUrl(downloadsPing, "downloadsStatus");
+window.onload = setLinks()
+window.onload = setButtonLinks()
