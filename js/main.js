5a51875ae9ba024568d147192ad22827d1852278
Evolve5164 <150050862+Evolve5164@users.noreply.github.com>
2024-03-11 12:37:48 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Set Urls
-const stream = "stream.deltanet.tk:54034"
-const request = "request.deltanet.tk:54034"
+const stream = "stream.deltanet.tk:58904"
+const request = "request.deltanet.tk:58904"
 const downloads = "jellyfin.org/downloads"
 
 // Set Urls to ping
